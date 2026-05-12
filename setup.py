ff35c0585010564d704002f673c9a9e1388bf1b5
Felix Koch <koch-felix@outlook.de>
2026-05-12 23:06:15 +0200

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='buildyn',
-    version='0.9',
+    version='1.0.0',
     packages=find_packages(),
     python_requires='==3.13.*',
 )
