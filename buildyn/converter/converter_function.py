d9bbb205262b6228b2482b8965b4b8653f5c2bca
Felix Koch <koch-felix@outlook.de>
2026-08-19 14:08:44 +0200

    Bugfix: Raise error not return error

--- buildyn/converter/converter_function.py
+++ buildyn/converter/converter_function.py
@@ -9,4 +9,4 @@ class ConverterFunction(ABC):
 
     def convert(self, var_dict: dict):
 
-        return NotImplementedError("Function convert not implemented in this ConverterFunction. Please override this method first.")
+        raise NotImplementedError("Function convert not implemented in this ConverterFunction. Please override this method first.")
